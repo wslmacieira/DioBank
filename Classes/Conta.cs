cf2d078a25d0e7f7a55c6f726fd9e7fa33d48904
wslmacieiradev <wslmacieira@outlook.com>
2021-03-04 01:00:53 -0300

    Criando Menu de operacoes

--- Classes/Conta.cs
+++ Classes/Conta.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Dio.Bank
 {
     public class Conta
@@ -13,6 +15,46 @@ namespace Dio.Bank
             Saldo = saldo;
             Credito = credito;
             Nome = nome;
+        }
+
+        public bool Sacar(double valorSaque)
+        {
+            // validação de saldo
+            if (Saldo - valorSaque < (Credito))
+            {
+                Console.WriteLine("Saldo insuficiente!");
+                return false;
+            }
+            Saldo -= valorSaque;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", Nome, Saldo);
+
+            return true;
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            Saldo += valorDeposito;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", Nome, Saldo);
+        }
+
+        public void Transferir(double valorTransferencia, Conta contaDestino)
+        {
+            if (Sacar(valorTransferencia))
+            {
+                contaDestino.Depositar(valorTransferencia);
+            }
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "TipoConta " + TipoConta + " | ";
+            retorno += "Nome " + Nome + " | ";
+            retorno += "Saldo " + Saldo + " | ";
+            retorno += "Crédito " + Credito;
+            return retorno;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,10 +6,62 @@ namespace Dio.Bank
     {
         static void Main(string[] args)
         {
-            Conta minhaConta = new Conta();
+            Conta minhaConta = new Conta(TipoConta.PessoaFisica, 0, 0, "Wslmacieira");
+            string opcaoUsuario = ObterOpcaoUsuario();
 
-            Console.WriteLine("Hello World!");
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //ListarContas();
+                        break;
+                    case "2":
+                        //InserirConta();
+                        break;
+                    case "3":
+                        //Transferir();
+                        break;
+                    case "4":
+                        //Sacar();
+                        break;
+                    case "5":
+                        //Depositar();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+
+            Console.WriteLine(minhaConta.ToString());
+
             Console.ReadLine();
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("DIO Bank a seu dispor!!!");
+            Console.WriteLine("Informe a opção desejada:");
+
+            Console.WriteLine("1- Listar contas");
+            Console.WriteLine("2- Inserir nova conta");
+            Console.WriteLine("3- Transferir");
+            Console.WriteLine("4- Sacar");
+            Console.WriteLine("5- Depositar");
+            Console.WriteLine("C- Limpar a tela");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
